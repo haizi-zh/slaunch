da5778621cc05eb75634f11993cbe4cbdd96f23f
Haizi Zheng <haizi.zh@gmail.com>
2021-06-03 11:02:53 -0400

    Regular update

--- slaunch.sh
+++ slaunch.sh
@@ -11,8 +11,8 @@ Options:\n
   -p|--partition\t\tPartition type. Default: RM. Other values: RM-shared, etc\n
   -J|--job-name\t\tThe name of the job\n
   -A|--account\t\tCluster charge ID\n
-  -t|--wall-time\t\tWall time. Must be in the format: HH:MM:SS\n
-  -T|--time-min\t\tMinimal wall time\n
+  -T|--wall-time\t\tWall time. Must be in the format: HH:MM:SS\n
+  -t|--time-min\t\tMinimal wall time\n
   --ntasks-per-node\tNumber of tasks per node. Usually there should be exactly one task per node\n
   -n|--ntasks\t\t\tThe total number of tasks over all nodes\n
   -w|--work-dir\t\tWorking directory\n
@@ -21,6 +21,7 @@ Options:\n
   --mail-addr\t\tEmail address for job event notifications\n
   --mail-type\t\tEmail notification types\n
   -d|--dry-run\t\tDry run without actually submitting the job\n
+  -s|--script\t\tSave the generated script to file\n
 "
 
 # Import config files from ~/.config/slaunch
@@ -29,151 +30,159 @@ if [ -f $config_file ]; then
   source $config_file
 fi
 
-while (( "$#" )); do
+while (("$#")); do
   case "$1" in
-    -p|--partition)
-      partition=$2
-      shift 2
-      ;;
-    -J|--job-name)
-      job_name=$2
-      shift 2
-      ;;
-    -A|--account)
-      account=$2
-      shift 2
-      ;;
-    -t|--wall-time)
-      wall_time=$2
-      shift 2
-      ;;
-    -T|--time-min)
-      time_min=$2
-      shift 2
-      ;;
-    --ntasks-per-node)
-      ntasks_per_node=$2
-      shift 2
-      ;;
-    -n|--ntasks)
-      ntasks=$2
-      shift 2
-      ;;
-    -w|--work-dir)
-      work_dir=$2
-      shift 2
-      ;;
-    --mail-addr)
-      mail_addr=$2
-      shift 2
-      ;;
-    --mail-type)
-      mail-type=$2
-      shift 2
-      ;;
-    -o|--output-dir)
-      output_dir=$2
-      shift 2
-      ;;
-    -P|--output-pattern)
-      output_pattern=$2
-      shift 2
-      ;;
-    -d|--dry-run)
-      dry_run=1
-      shift
-      ;;
-    -h|--help)
-      echo -e $help_msg
-      exit 0
-      ;;
-    --) # end argument parsing
-      shift
-      PARAMS="$PARAMS $*"
-      break
-      ;;
-    -*|--*=) # unsupported flags
-      echo "Error: Unsupported flag $1" >&2
-      exit 1
-      ;;
-    *) # preserve positional arguments
-      PARAMS="$PARAMS $1"
-      shift
-      ;;
+  -p | --partition)
+    partition=$2
+    shift 2
+    ;;
+  -J | --job-name)
+    job_name=$2
+    shift 2
+    ;;
+  -A | --account)
+    account=$2
+    shift 2
+    ;;
+  -t | --wall-time)
+    wall_time=$2
+    shift 2
+    ;;
+  -T | --time-min)
+    time_min=$2
+    shift 2
+    ;;
+  --ntasks-per-node)
+    ntasks_per_node=$2
+    shift 2
+    ;;
+  -n | --ntasks)
+    ntasks=$2
+    shift 2
+    ;;
+  -w | --work-dir)
+    work_dir=$2
+    shift 2
+    ;;
+  --mail-addr)
+    mail_addr=$2
+    shift 2
+    ;;
+  --mail-type)
+    mail-type=$2
+    shift 2
+    ;;
+  -o | --output-dir)
+    output_dir=$2
+    shift 2
+    ;;
+  -P | --output-pattern)
+    output_pattern=$2
+    shift 2
+    ;;
+  -d | --dry-run)
+    dry_run=1
+    shift
+    ;;
+  -s | --script)
+    save_script=$2
+    shift 2
+    ;;
+  -h | --help)
+    echo -e $help_msg
+    exit 0
+    ;;
+  --) # end argument parsing
+    shift
+    PARAMS="$PARAMS $*"
+    break
+    ;;
+  -* | --*=) # unsupported flags
+    echo "Error: Unsupported flag $1" >&2
+    exit 1
+    ;;
+  *) # preserve positional arguments
+    PARAMS="$PARAMS $1"
+    shift
+    ;;
   esac
 done
 # set positional arguments in their proper place
 set -- "$PARAMS"
 
 # Write the temporary SLURM job-submission file
-out_file_id=$(date +%s)_$RANDOM
-out_file="/tmp/slurm_tmp.$out_file_id.sh"
-echo "#!/bin/bash" > $out_file
-echo "#SBATCH -N 1" >> $out_file
-echo "#SBATCH -C EGRESS" >> $out_file
+out_file=$(mktemp --suffix .sh)
+echo "#!/bin/bash" >$out_file
+echo "#SBATCH -N 1" >>$out_file
+echo "#SBATCH -C EGRESS" >>$out_file
 
 if [ ! -z $partition ]; then
-  echo "#SBATCH -p $partition" >> $out_file
+  echo "#SBATCH -p $partition" >>$out_file
 fi
 
 if [ ! -z $job_name ]; then
-  echo "#SBATCH --job-name $job_name" >> $out_file
+  echo "#SBATCH --job-name $job_name" >>$out_file
 fi
 
 if [ ! -z $account ]; then
-  echo "#SBATCH --account $account" >> $out_file
+  echo "#SBATCH --account $account" >>$out_file
 fi
 
 if [ ! -z $wall_time ]; then
-  echo "#SBATCH -t $wall_time" >> $out_file
+  echo "#SBATCH --time $wall_time" >>$out_file
 fi
 
 if [ ! -z $time_min ]; then
-  echo "#SBATCH --time-min $time_min" >> $out_file
+  echo "#SBATCH --time-min $time_min" >>$out_file
 fi
 
 if [ ! -z $ntasks_per_node ]; then
-  echo "#SBATCH --ntasks-per-node $ntasks_per_node" >> $out_file
+  echo "#SBATCH --ntasks-per-node $ntasks_per_node" >>$out_file
 fi
 
 if [ ! -z $ntasks ]; then
-  echo "#SBATCH --ntasks $ntasks" >> $out_file
+  echo "#SBATCH --ntasks $ntasks" >>$out_file
 fi
 
 if [ ! -z $mail_addr ]; then
-  echo "#SBATCH --mail-user $mail_addr" >> $out_file
+  echo "#SBATCH --mail-user $mail_addr" >>$out_file
 fi
 
 if [ ! -z $mail_type ]; then
-  echo "#SBATCH --mail-type $mail_type" >> $out_file
+  echo "#SBATCH --mail-type $mail_type" >>$out_file
 fi
 
 if [ ! -z $output_dir ]; then
   if [ -z $output_pattern ]; then
     output_pattern="%j-%x"
   fi
-  echo "#SBATCH --output $output_dir/$output_pattern.log" >> $out_file
-  echo "#SBATCH --error $output_dir/$output_pattern.err" >> $out_file
+  echo "#SBATCH --output $output_dir/$output_pattern.log" >>$out_file
+  echo "#SBATCH --error $output_dir/$output_pattern.err" >>$out_file
 fi
 
 if [ ! -z $work_dir ]; then
-  echo "#SBATCH --chdir $work_dir" >> $out_file
+  echo "#SBATCH --chdir $work_dir" >>$out_file
 fi
 
-echo -e '\nset -euo pipefail' >> $out_file
-echo -e 'IFS=$'"'"'\\n\\t'"'" >> $out_file
-echo -e "set -x" >> $out_file
+echo -e '\nset -euo pipefail' >>$out_file
+echo -e 'IFS=$'"'"'\\n\\t'"'" >>$out_file
+echo -e "set -x" >>$out_file
 
-echo "" >> $out_file
-echo $PARAMS >> $out_file
+echo "" >>$out_file
+echo -e $PARAMS >>$out_file
+echo ""
 
-echo -e "SLURM script generated:\n"
-echo -e "========"
+if [ ! -z $save_script ]; then
+  cp -a $out_file $save_script
+fi
+
+# echo -e "SLURM script generated:\n"
+# echo -e "========"
 
 cat $out_file
 
-echo -e "========\n"
-echo -e "SLURM script ends here\n"
+# echo -e "========\n"
+# echo -e "SLURM script ends here\n"
 
 if [ -z $dry_run ]; then
   sbatch_output=$(sbatch $out_file)
@@ -181,9 +190,9 @@ if [ -z $dry_run ]; then
 
   if [ ! -z $output_dir ]; then
     job_id=$(echo $sbatch_output | sed -e 's/Submitted batch job \([0-9]\+\).*/\1/')
-    cat $out_file > "$output_dir/slurm-$job_id.sh"
+    cp -a $out_file $output_dir/$output_pattern.sh
   fi
 fi
 
-echo "Removing $out_file ..."
+# echo "Removing $out_file ..."
 rm $out_file
